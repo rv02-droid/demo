70cfc38157b17a6442e22b8908c1240300febba5
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-14 14:05:47 +0530

    Initial release test deploye setup

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from demoapp";
+        return "HELLO FORM DEMO CHECK ";
     }
 
     @GetMapping("/health")
