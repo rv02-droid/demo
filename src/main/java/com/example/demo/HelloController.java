a1ad93260a4ed8ee9a569ec640611d072d52e71c
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-14 15:37:59 +0530

    Hello world

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "HELLO FORM DEMO check  ";
+        return "HELLO WORLD";
     }
 
     @GetMapping("/health")
