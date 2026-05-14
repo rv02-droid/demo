cc8fe448985f2bbe43a5572b743fb7c1ba8e664f
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-14 13:52:24 +0530

    Updated welcome message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from demoapp";
+        return "HeLLO FROM DEMO";
     }
 
     @GetMapping("/health")
