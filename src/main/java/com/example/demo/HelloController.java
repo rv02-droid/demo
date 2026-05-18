ed5c72f8f2d881bac25eb99c3633a008c92f4f4d
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-18 13:51:33 +0530

    Change port and remove hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "HELLO WORlDD..!";
+        return "WORlDD..!";
     }
 
     @GetMapping("/health")
