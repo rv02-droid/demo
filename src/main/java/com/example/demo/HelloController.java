3cfa35dd9d104ebdb95131ec571b0227ea666219
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-18 16:20:06 +0530

    Inital commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello WORlD..!";
+        return "Hello WO..!";
     }
 
     @GetMapping("/health")
