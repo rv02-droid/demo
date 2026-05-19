9dc25b8effb0778809fb797a99bb9422ea625110
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-19 16:29:14 +0530

    Initial commit??..

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Higest version jar..... ";
+        return "Higest version is 0.0.2-SNAPSHOT";
     }
 
     @GetMapping("/health")
