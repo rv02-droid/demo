b366e9e8b8f1240ced21feff8733af7d2644cca1
Rishabh Verma <rishabhverma0232@gmail.com>
2026-05-28 13:25:18 +0530

    Check for automated intallation of scripts

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello veriosn 0.0.8 here with new changes";
+        return "Hello world of version 1";
     }
 
     @GetMapping("/health")
