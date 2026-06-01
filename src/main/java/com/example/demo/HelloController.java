f34f11daddb3620c3cef9101b7b081b1c8db4ddc
Rishabh Verma <rishabhverma0232@gmail.com>
2026-06-01 23:57:51 +0530

    hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello version ..... 0.0.12";
+        return "Hello version ..... 0.0.19";
     }
 
     @GetMapping("/health")
